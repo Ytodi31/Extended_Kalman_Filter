8fd4640e301c56d0c2385be9cc8871667e99e5d5
Ytodi31 <yashaarth@todi89.com>
2020-04-23 10:31:48 -0400

    Added computation of Jacobian for measurement model

--- ekf.py
+++ ekf.py
@@ -42,3 +42,38 @@ def wraptopi(x):
     elif x < -np.pi:
         x = x + (np.floor(x / (-2 * np.pi)) + 1) * 2 * np.pi
     return x
+
+
+
+
+def measurement_update(lk, rk, bk, P_check, x_check):
+    
+    # 1. Compute measurement Jacobian
+    x_check[2] = wraptopi(x_check[2])
+    r = np.sqrt((lk[0] - x_check[0] - d*np.cos(x_check[2]))**2 + (lk[1] - x_check[1] - d*np.sin(x_check[2]))**2) 
+    phi = np.arctan2(lk[1] - x_check[1] - d*np.sin(x_check[2]), lk[0] - x_check[0] - d*np.cos(x_check[2])) - x_check[2]
+    phi = wraptopi(phi)
+    yk = np.reshape(np.array([r, phi]), (1,2)) 
+    n = np.random.multivariate_normal([0, 0], cov_y, 1)
+    yk = yk
+
+    dh1_dx1 = -(lk[0] - x_check[0] - d*np.cos(x_check[2]))/r
+    dh1_dx2 = -(lk[1] - x_check[1] - d*np.sin(x_check[2]))/r
+    dh1_dx3 = ((lk[0] -  x_check[0] - d*np.cos(x_check[2]))*np.sin(x_check[2]) + 
+               (lk[1] - x_check[1] - d*np.sin(x_check[2]))*-np.cos(x_check[2]))*d/r
+    dh2_dx1 = (lk[1] - x_check[1] - d*np.sin(x_check[2]))/r**2
+    dh2_dx2 = -(lk[0] -  x_check[0] - d*np.cos(x_check[2]))/r**2
+    dh2_dx3 = -((lk[1] - x_check[1] - d*np.sin(x_check[2]))*d*np.sin(x_check[2]) + 
+                  (lk[0] -  x_check[0] - d*np.cos(x_check[2]))*d*np.cos(x_check[2]))/r**2
+
+    H = np.array([[dh1_dx1, dh1_dx2, dh1_dx3], [dh2_dx1, dh2_dx2, dh2_dx3]])
+    H = np.reshape(H, (2,3))
+    M = np.eye(2)
+
+    # 2. Compute Kalman Gain
+
+    # 3. Correct predicted state
+
+    # 4. Correct covariance
+	
+    return x_check, P_check
